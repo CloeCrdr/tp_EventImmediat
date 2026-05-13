685d0275eec2a08aef1696849e3169e70e733cac
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2026-05-13 15:02:11 +0200

    Update settings.py

--- tp_EventImmediat/settings.py
+++ tp_EventImmediat/settings.py
@@ -127,16 +127,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 MEDIA_URL = '/media/'
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'LiveApp', 'static', 'media')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'LiveApp', 'static'),
 ]
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
