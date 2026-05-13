581264b41d8fef1bd1a76f3605e78c0ac5f15ca8
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2026-05-13 14:48:25 +0200

    Update settings.py

--- tp_EventImmediat/settings.py
+++ tp_EventImmediat/settings.py
@@ -80,7 +80,7 @@ EMAIL_PORT = 1025
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
